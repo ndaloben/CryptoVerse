33a820cbeb175bb39f8aa054be4bcb933ca6ce34
ben <ndaloben8@gmail.com>
2023-05-15 13:18:11 +0300

    statistical data added

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import millify from 'millify'
+import { Typography, Row, Col, Statistic } from 'antd'
 
 const Homepage = () => {
+const { Title } = Typography;
   return (
-    <div>
-      homepage
-    </div>
+    <>
+      <Title level={2} className='heading'>Global Crypto Stats</Title>
+      <Row>
+        <Col span={12}><Statistic title="Total Cryptocurrencies" value="5" /></Col>
+        <Col span={12}><Statistic title="Total Exchanges" value="5" /></Col>
+        <Col span={12}><Statistic title="Total Market Cap" value="5" /></Col>
+        <Col span={12}><Statistic title="Total 24h Volume" value="5" /></Col>
+        <Col span={12}><Statistic title="Total Markets" value="5" /></Col>
+      </Row>
+    </>
   )
 }
 
